3d2df4c1411efc49b6c3584becd98af536647a2c
M.Charattrakool <m.charattrakool@gmail.com>
2016-10-07 13:28:26 +0700

    broken

--- core/src/com/monthol/game/pacman/Pacman.java
+++ core/src/com/monthol/game/pacman/Pacman.java
@@ -2,12 +2,14 @@ package com.monthol.game.pacman;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Pacman extends ApplicationAdapter {
-	public SpriteBatch batch;
+	SpriteBatch batch;
+
     
     @Override
     public void create () {
