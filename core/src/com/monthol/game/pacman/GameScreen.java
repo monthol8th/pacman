97eda7e424a28fe0b91bf066fc61d04dcc0a0def
M.Charattrakool <m.charattrakool@gmail.com>
2016-10-07 13:43:47 +0700

    add Image

--- core/src/com/monthol/game/pacman/GameScreen.java
+++ core/src/com/monthol/game/pacman/GameScreen.java
@@ -1,17 +1,25 @@
 package com.monthol.game.pacman;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
  
     private Pacman pacmanGame;
-    
+    private Texture pacmanImg;
+
     public GameScreen(Pacman pacmanGame) {
         this.pacmanGame = pacmanGame;
+        pacmanImg = new Texture("pacman.png");
+
     }
     
     @Override
     public void render(float delta) {
-        System.out.println("Hello " + delta);
+        SpriteBatch batch = pacmanGame.batch;
+        batch.begin();
+        batch.draw(pacmanImg, 100, 100);
+        batch.end();
     }
 }
