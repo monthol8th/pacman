1c0c789e3374ec3695b16e52b2a1c28f21f68b20
M.Charattrakool <m.charattrakool@gmail.com>
2016-10-07 14:30:14 +0700

    create Maze

--- core/src/com/monthol/game/pacman/World.java
+++ core/src/com/monthol/game/pacman/World.java
@@ -3,14 +3,23 @@ package com.monthol.game.pacman;
 public class World {
     private Pac pacman;
     private Pacman pacmanGame;
- 
+    private Maze maze;
+
+    
     World(Pacman pacmanGame) {
         this.pacmanGame = pacmanGame;
  
         pacman = new Pac(100,100);
+        maze = new Maze();
+
     }
  
     Pac getPacman() {
         return pacman;
     }
+
+    Maze getMaze() {
+        return maze;
+    }
+
 }
